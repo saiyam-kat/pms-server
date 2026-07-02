22d744c277e636cbd745745fa351d8abc4272ca2
saiyam katiyar <saiyam30173@gmail.com>
2026-07-02 22:53:51 +0530

    fix: update CORS configuration to allow multiple origins

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['
+    http://localhost:5173',
+    'https://pms-client-beige.vercel.app'
+  ],
   credentials: true
 }));
 
